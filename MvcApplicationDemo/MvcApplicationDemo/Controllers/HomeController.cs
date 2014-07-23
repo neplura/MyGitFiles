667c6029a8153d428d0fb8bf6806b7f64c37981d
Hai <neplura@hotmail.com>
2014-07-23 15:59:26 +0800

    ASP.net MVC Demo
    Exploration.

---
+++ MvcApplicationDemo/MvcApplicationDemo/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplicationDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "欢迎使用 ASP.NET MVC!";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+        public ActionResult Form()
+        {
+            ViewBag.Message = "表单输入页!";
+            
+            return View();
+        }
+    }
+}
+
